dd0b7fd3c0d1cadf95ba6b9c3391bae5585aec29
andrey1510 <andrey.ivn15@gmail.com>
2025-05-24 23:49:52 +0300

    updated

---
+++ my-microservices-app/src/main/java/com/mymicroservicesapp/MyMicroservicesAppApplication.java
@@ -0,0 +1,13 @@
+package com.mymicroservicesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroservicesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMicroservicesAppApplication.class, args);
+    }
+
+}
